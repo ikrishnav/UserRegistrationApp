c98f4622981ae4194e13b34c3b9b49d73d22b9bb
ikrishnav <134567148+ikrishnav@users.noreply.github.com>
2023-07-28 00:35:57 +0800

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,14 +1,5 @@
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
-public class FirstSchDay {
-
-	public static void main(String[] args) {
-		LocalDateTime firstDayOfSch = LocalDateTime.of(2019, 4, 15, 9, 15);
-		
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE dd MMMM yyyy, hhmm a");
-		String datetime_=firstDayOfSch.format(formatter);
-		System.out.println("The first day of school is on " +
-		firstDayOfSch.format(formatter));
-		} }
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
